ddf9c8a3fb89374e0be290d49466c9cf0c783071
Erik Raith <raithhtiar@gmail.com>
2020-04-22 10:20:44 -0600

    [test] Add orders.test.js

---
+++ src/reducers/orders.test.js
@@ -0,0 +1,42 @@
+import {orders} from './orders';
+
+
+
+describe("Reducer Tests", () => {
+  let mockOrders,mockOrder;
+  beforeEach(()=>{
+    mockOrders = [
+      { id: 1, name: "Jill", ingredients: [] },
+      { id: 2, name: "Hill", ingredients: [] },
+    ];
+
+    mockOrder = { id: 1, name: "Jill", ingredients: [] };
+  })
+
+  
+  it("Should set the correct state using the SET_ORDERS action", () => {
+    let mockOrdersAction = {
+      type:"SET_ORDERS",
+      orders:mockOrders
+    }
+    
+    let result = orders([],mockOrdersAction)
+
+    expect(result).toEqual(mockOrders)
+  })
+
+  it("Should set the correct state using the ADD_ORDER action", () => {
+    let mockOrderAction = {
+      type:"ADD_ORDER",
+      order:mockOrder
+    }
+    let mockExpectedState = [{ id: 1, name: "Jill", ingredients: [] }];
+
+    let result = orders([],mockOrderAction)
+
+    expect(result).toEqual(mockExpectedState)
+  })
+
+})
+
+
